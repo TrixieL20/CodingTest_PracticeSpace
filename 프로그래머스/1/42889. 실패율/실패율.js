1d586c44e7998bde0b5b46453b936a00b49439f0
TrixieL20 <minjung0768@gmail.com>
2025-05-19 20:32:47 +0900

    [level 1] Title: 실패율, Time: 46.47 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,22 @@
+function solution(N, stages) {
+    let challenger = new Array(N + 2).fill(0);
+    for (const stage of stages) {
+        challenger[stage] += 1;
+    }
+    
+    let fails = {};
+    let total = stages.length;
+    
+    for (let i = 1; i <= N; i++) {
+        if (challenger[i] === 0) {
+            fails[i] = 0;
+            continue;
+        }
+        
+        fails[i] = challenger[i] / total;
+        total -= challenger[i];
+    }
+    
+    const result = Object.entries(fails).sort((a, b) => b[1] - a[1]);   
+    return result.map((v) => Number(v[0]));
+}
