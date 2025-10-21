507f1b86d54b941487881b9f25aea2f9fbf89fac
TrixieL20 <minjung0768@gmail.com>
2025-10-21 15:20:00 +0900

    [Gold III] Title: 불!, Time: 80 ms, Memory: 10872 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -11,8 +11,8 @@ const int dx[] = {0, 0, 1, -1}, dy[] = {1, -1, 0, 0};
  
 void solution(int j_y, int j_x) {
 	
-	// 🔥 불 번짐 BFS
-	while (!q.empty()) {
+	// 불 번짐 
+	while (q.size() > 0) {
 		int f_y, f_x;
 		tie(f_y, f_x) = q.front(); q.pop();
 		
@@ -20,36 +20,30 @@ void solution(int j_y, int j_x) {
 			int nfy = f_y + dy[i], nfx = f_x + dx[i];
 			
 			if (nfy < 0 || nfy >= r || nfx < 0 || nfx >= c) continue;
-			if (m[nfy][nfx] == '#' || fired[nfy][nfx] != INF) continue;  // ✅ 수정된 부분
+			if (m[nfy][nfx] == '#' || fired[nfy][nfx] != INF) continue;
 			
 			fired[nfy][nfx] = fired[f_y][f_x] + 1;
 			q.push({nfy, nfx});
 		}
 	}
 	
-	// 👤 지훈이의 이동 BFS
-	queue<pair<int,int>> jq;
-	jq.push({j_y, j_x});
-	visited[j_y][j_x] = 1;
-
-	while (!jq.empty()) {
-		tie(j_y, j_x) = jq.front(); jq.pop();
+	// 지훈이의 이동
+	q.push({j_y, j_x}); visited[j_y][j_x] = 1;
+	while (q.size() > 0) {
+		tie(j_y, j_x) = q.front(); q.pop();
 		
-		// 탈출 조건: 가장자리에 도달
 		if (j_y == 0 || j_y == r - 1 || j_x == 0 || j_x == c - 1) {
 			answer = visited[j_y][j_x];
-			return;
+			break;
 		}
 		
 		for (int i = 0; i < 4; i++) {
 			int njy = j_y + dy[i], njx = j_x + dx[i];
 				
-			if (njy < 0 || njy >= r || njx < 0 || njx >= c) continue;
-			if (m[njy][njx] == '#' || visited[njy][njx] != INF) continue;
-			if (fired[njy][njx] <= visited[j_y][j_x] + 1) continue; // 🔥 불보다 빨리 가야함
+			if (m[njy][njx] == '#' || m[njy][njx] == 'F' || fired[njy][njx] <= visited[j_y][j_x] + 1 || visited[njy][njx] != INF) continue;
 				
 			visited[njy][njx] = visited[j_y][j_x] + 1;
-			jq.push({njy, njx});
+			q.push({njy, njx});
 		}
 	}
 }
@@ -61,13 +55,14 @@ int main() {
 	fired.assign(r, vector<int>(c, INF));
 	
 	for (int i = 0; i < r; i++) {
+		vector<int> v;
 		for (int j = 0; j < c; j++) {
 			cin >> m[i][j];
 			
-			if (m[i][j] == 'J') {
+			if(m[i][j] == 'J') {
 				j_y = i;
 				j_x = j;
-			} else if (m[i][j] == 'F') {
+			} else if(m[i][j] == 'F') {
 				q.push({i, j});
 				fired[i][j] = 1;
 			}
@@ -76,6 +71,9 @@ int main() {
 	
 	solution(j_y, j_x);
 	
-	if (answer == INF) cout << "IMPOSSIBLE\n";
-	else cout << answer << "\n";
+	if (answer == INF) {
+		cout << "IMPOSSIBLE";
+	} else {
+		cout << answer;
+	}
 }
