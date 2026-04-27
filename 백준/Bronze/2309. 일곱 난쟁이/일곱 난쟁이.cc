3316f98f2c76ea920461d52d98778f59491948d4
TrixieL20 <minjung0768@gmail.com>
2026-04-28 07:15:46 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,31 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;  
-int a[9], sum;
-vector<int> v; 
-pair<int, int> ret; 
-void solve(){
-	for(int i = 0; i < 9; i++){
-		for(int j = 0; j < i; j++){
-			if(sum - a[i] - a[j] == 100){
-				ret = {i, j};
-				return;
-			}
-		}
-	}
-}
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    for(int i = 0; i < 9; i++){
-        cin >> a[i]; sum += a[i];
+#include<bits/stdc++.h>
+using namespace std;
+int a[9]; 
+int n = 9, r = 7; 
+void solve(){  
+    int sum = 0; 
+    for(int i = 0; i < r; i++){
+        sum += a[i];  
+    }
+    if(sum == 100){ 
+      sort(a, a + 7);  
+      for(int i = 0; i < r; i++) cout << a[i] << "\n"; 
+      exit(0);
     } 
-	solve();
-	for(int i = 0; i < 9; i++){
-		if(ret.first == i || ret.second == i) continue;
-		v.push_back(a[i]);
-	}
-	sort(v.begin(), v.end());
-	for(int i : v) cout << i << " "; 
+}
+void print(){
+  for(int i = 0; i < r; i++) cout << a[i] << " "; 
+  cout << '\n';
+}
+void makePermutation(int n, int r, int depth){
+    if(r == depth){ 
+        solve();
+        return;
+    }
+    for(int i = depth; i < n; i++){
+        swap(a[i], a[depth]);
+        makePermutation(n, r, depth + 1);
+        swap(a[i], a[depth]);
+    }
+    return;
+}
+int main(){
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    } 
+    makePermutation(n, r, 0);
     return 0;
 }
